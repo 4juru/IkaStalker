10240bea48cbaf737c3499b1ca6a066ce1f79d1e
4juru <4jurun@gmail.com>
2017-05-08 01:30:15 +0900

    clean

--- ikamenu.1m.rb
+++ ikamenu.1m.rb
@@ -11,194 +11,148 @@ load 'lib/login.txt'
 
 
 
+private
+class String
+	# テキスト変換
+	def mode_to_text
+		case self.to_s
+			when "online"  ; "●　　　"
+			when "playing" ; "プレイ中"
+			when "tag"     ; "タッグ　"
+			when "gachi"   ; "ガチ　　"
+			when "private" ; "プラベ　"
+			when "regular" ; "ナワバリ"
+		end + "  "
+	end
+	def mode_to_color
+		case self.to_s
+			when "online"  ; "gray"
+			when "playing" ; "black"
+			when "tag"     ; "orange"
+			when "gachi"   ; "red"
+			when "private" ; "blue"
+			when "regular" ; "green"
+		end
+	end
+	def rule_to_color
+		case self.to_s
+			when "ガチエリア"	; "orange"
+			when "ガチホコ"	; "red"
+			when "ガチヤグラ"	; "blue"
+			else 			; "green"
+		end
+	end
+	def hour
+		Time.parse(self.to_s).hour.to_s + ":00"
+	end
 
-def timeconv(t)
-	return Time.parse(t).hour.to_s + ":00"
-end
+	# テキスト修飾メソッド
+	def _size(s)
+		self.to_s + " size=" + s.to_s
+	end
+	def _color(c)
+		self.to_s + " color=" + c
+	end
+	def _uri(u)
+		self.to_s + " href=" + u
+	end
+	def _img(i)
+		self.to_s + " image=" + i
+	end
+	def opt
+		self.to_s + " |"
+	end
 
-def dayconv(t)
-	return Time.parse(t).month.to_s+  "/" +Time.parse(t).day.to_s 
-end
-
-def mode2color(mode)
-	if mode=="online" then
-		return "gray"
-	elsif mode=="playing" then
-		return "black"
-	elsif mode=="tag" then
-		return "orange"
-	elsif mode=="gachi" then
-		return "red"
-	elsif mode=="private" then
-		return "blue"
-	elsif mode=="regular" then
-		return "green"
-	else
-		return "blue"
+	# 情報表示用メソッド
+	def rule_view
+		self.to_s.opt._size(16)._color(self.to_s.rule_to_color)
+	end
+	def map_view
+		self.to_s.opt._size(20)._color("black")
+	end
+	def img_view(img)
+		self.to_s.opt._img($stage_img[ img ])
 	end
 end
 
-def mode2char(mode)
-	if mode=="online" then
-		return "●　　　"
-	elsif mode=="playing" then
-		return "プレイ中"
-	elsif mode=="tag" then
-		return "タッグ　"
-	elsif mode=="gachi" then
-		return "ガチ　　"
-	elsif mode=="private" then
-		return "プラベ　"
-	elsif mode=="regular" then
-		return "ナワバリ"
-	else
-		return mode
-	end
-end
 
-def rule2color(rule)
-	if rule=="ガチエリア" then
-		return "orange"
-	elsif rule=="ガチホコ" then
-		return "red"
-	elsif rule=="ガチヤグラ" then
-		return "blue"
-	else
-		return "green"
-	end
-end
 
 
 # [URI]
-uri = "https://splapi.fetus.jp/schedule"
+splapi_uri = "https://splapi.fetus.jp/schedule"
+friends_uri = 'https://splatoon.nintendo.net/friend_list/index.json'
+prof_uri = "https://splatoon.nintendo.net/profile/"
+howto_s_uri = "https://github.com/4juru/IkaStalker/blob/master/lib/getcookie.md"
+howto_h_uri = "https://github.com/4juru/IkaStalker/blob/master/lib/sethttparty.md"
 
 
-# [GETリクエスト]
-# Net::HTTP.startでHTTPセッションを開始する
-# 既にセッションが開始している場合はIOErrorが発生
-response = Net::HTTP.get_response(URI.parse(uri))
+# splapiから情報取得
+response = Net::HTTP.get_response(URI.parse(splapi_uri))
+res_h = (JSON.parse(response.body))["result"]
 
-# [JSONパース処理]
-# JSONオブジェクトをHashへパースする
-# JSON::ParserErrorが発生する可能性がある
-jhash = JSON.parse(response.body)
 
-head=["","--","--"]
-reg_rule=[]
-gachi_rule=[]
-time=Array.new(3){Hash.new()}
-day=[]
-gachi_map=Array.new(3){ Array.new(2) }
-reg_map=Array.new(3){ Array.new(2) }
+# メニューのアイコン表示
+t=0
+puts 'ᔦꙬᔨ-' + res_h["regular"][t]["end"].hour
+puts '---'
 
-gachi_mapx=Array.new(3){ Array.new(2) }
-reg_mapx=Array.new(3){ Array.new(2) }
 
-0.upto(2) do |i|
-	time[i]["start"] = timeconv(jhash["result"]["regular"][i]["start"])
-	time[i]["end"] = timeconv(jhash["result"]["regular"][i]["end"])
-	day[i] = dayconv(jhash["result"]["regular"][i]["start"])
-	reg_rule[i]= "ナワバリ"
-	gachi_rule[i]= jhash["result"]["gachi"][i]["rule"]
-	reg_map[i][0]= jhash["result"]["regular"][i]["maps"][0]
-	reg_map[i][1]= jhash["result"]["regular"][i]["maps"][1]
-	gachi_map[i][0]= jhash["result"]["gachi"][i]["maps"][0]
-	gachi_map[i][1]= jhash["result"]["gachi"][i]["maps"][1]
-
-	reg_mapx[i][0] = jhash["result"]["regular"][i]["maps_ex"][0]["statink"]
-	reg_mapx[i][1] = jhash["result"]["regular"][i]["maps_ex"][1]["statink"]
-	gachi_mapx[i][0] = jhash["result"]["gachi"][i]["maps_ex"][0]["statink"]
-	gachi_mapx[i][1] = jhash["result"]["gachi"][i]["maps_ex"][1]["statink"]
+# 現在のステージ情報表示
+for mode in ["regular","gachi"] do
+	puts res_h[mode][t]["rule"].rule_view
+	for itr in [0,1] do
+		puts res_h[mode][t]["maps"][itr].map_view
+		puts '--'.img_view(res_h[mode][t]["maps_ex"][itr]["statink"])
+	end
 end
 
 
-t=0
-
-puts 'ᔦꙬᔨ' +"-"+ time[t] ["end"]
-# puts 'ᔦꙬᔨ'
+# 次のステージ情報表示
 puts '---'
+puts '次のステージ'.opt._size(18)
 
-puts head[t] + reg_rule[t] + '| size=16 color=' + rule2color(reg_rule[t])
-puts head[t] + reg_map[t][0] + "| size=20 color=black"
-puts head[t] + '--| image="' + $stage_img[reg_mapx[t][0]] + '"'
-puts head[t] + reg_map[t][1] + "| size=20 color=black"
-puts head[t] + '--| image="' + $stage_img[reg_mapx[t][1]] + '"'
-
-puts head[t] + gachi_rule[t] + '| size=16 color=' + rule2color(gachi_rule[t])
-puts head[t] + gachi_map[t][0] + "| size=20 color=black"
-puts head[t] + '--| image="' + $stage_img[gachi_mapx[t][0]] + '"'
-puts head[t] + gachi_map[t][1] + "| size=20 color=black"
-puts head[t] + '--| image="' + $stage_img[gachi_mapx[t][1]] + '"'
-puts '---'
-
-puts '次のステージ| size=16'
-
-t=1
-
-puts time[t] ["start"] + "-" + time[t] ["end"] + '| size=18'
-
-puts head[t] + reg_rule[t] + '| size=16 color=' + rule2color(reg_rule[t])
-puts head[t] + reg_map[t][0] + "| size=20 color=black"
-puts head[t] + '--| image="' + $stage_img[reg_mapx[t][0]] + '"'
-puts head[t] + reg_map[t][1] + "| size=20 color=black"
-puts head[t] + '--| image="' + $stage_img[reg_mapx[t][1]] + '"'
-
-puts head[t] + gachi_rule[t] + '| size=16 color=' + rule2color(gachi_rule[t])
-puts head[t] + gachi_map[t][0] + "| size=20 color=black"
-puts head[t] + '--| image="' + $stage_img[gachi_mapx[t][0]] + '"'
-puts head[t] + gachi_map[t][1] + "| size=20 color=black"
-puts head[t] + '--| image="' + $stage_img[gachi_mapx[t][1]] + '"'
+for t in [1,2] do
+	puts res_h["regular"][t]["start"].hour + "-" + res_h["regular"][t]["end"].hour.opt._size(18)
+	for mode in ["regular","gachi"] do
+		puts '--' + res_h[mode][t]["rule"].rule_view
+		for itr in [0,1] do
+			puts '--' + res_h[mode][t]["maps"][itr].map_view
+			puts '--'.img_view(res_h[mode][t]["maps_ex"][itr]["statink"])
+		end
+	end
+end
 
 
-t=2
-
-puts time[t] ["start"] + "-" + time[t] ["end"] + '| size=18'
-
-puts head[t] + reg_rule[t] + '| size=16 color=' + rule2color(reg_rule[t])
-puts head[t] + reg_map[t][0] + "| size=20 color=black"
-puts head[t] + '--| image="' + $stage_img[reg_mapx[t][0]] + '"'
-puts head[t] + reg_map[t][1] + "| size=20 color=black"
-puts head[t] + '--| image="' + $stage_img[reg_mapx[t][1]] + '"'
-
-puts head[t] + gachi_rule[t] + '| size=16 color=' + rule2color(gachi_rule[t])
-puts head[t] + gachi_map[t][0] + "| size=20 color=black"
-puts head[t] + '--| image="' + $stage_img[gachi_mapx[t][0]] + '"'
-puts head[t] + gachi_map[t][1] + "| size=20 color=black"
-puts head[t] + '--| image="' + $stage_img[gachi_mapx[t][1]] + '"'
-
+# フレンドの状態表示
 begin
-  require 'httparty'
+	require 'httparty'
 
 	if $wag_session!="skip"
 		puts '---'
-		puts 'Friends | size=18'
+		puts 'Friends'.opt._size(18)
 
-
-		res = HTTParty.get('https://splatoon.nintendo.net/friend_list/index.json', headers: { 'Cookie' => "_wag_session=#{$wag_session}"} )
-
-
+		res = HTTParty.get(friends_uri, headers: { 'Cookie' => "_wag_session=#{$wag_session}"} )
 
 		if res.success?
 			res.sort_by {|h| h['mode'] }.each do |h|
-			  puts mode2char(h['mode']) + "   #{h['mii_name']}" + " | size=20 color=" + mode2color(h['mode'])+ " href=https://splatoon.nintendo.net/profile/#{h['hashed_id']}"
+				puts h['mode'].mode_to_text + "#{h['mii_name']}".opt._size(20)._color( h['mode'].mode_to_color )._uri( prof_uri+"#{h['hashed_id']}" )
 			end
 		else
 			case res.code
-			  when 200
-			    puts "All good!"
 			  when 401
-			    puts "wag_sessionが正しくない可能性があります | color=red"
-			    puts "設定方法 | color=blue href=https://github.com/4juru/IkaStalker/blob/master/lib/getcookie.md"
+			    puts "wag_sessionが正しくない可能性があります".opt._color("red")
+			    puts "設定方法".opt._color("blue")._uri(howto_s_uri)
 			  when 404
 			    puts "O noes not found!"
 			  when 500...600
-			    puts "ZOMG ERROR #{response.code}"
+			    puts "ZOMG ERROR #{res.code}"
 			end
 		end
 	end
 
 rescue LoadError
 	puts "---"
-	puts "httpartyがインストールされていません| color=red"
-	puts "インストール方法 | color=blue href=https://github.com/4juru/IkaStalker/blob/master/lib/sethttparty.md"
+	puts "httpartyがインストールされていません".opt._color("red")
+	puts "インストール方法"opt._color("blue")._uri(howto_h_uri)
 end
+
